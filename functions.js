6990d044f5c2882ae8ca734a4cff1cb11f5b0085
NikolayK <nikolay.kim@codeimmersive.com>
2019-12-04 15:59:54 -0500

    fixes add button

--- functions.js
+++ functions.js
@@ -1,8 +1,13 @@
-document.querySelector('.add-todo').addEventListener('click', printTodo(function(){
-    return todo = {
-        text: document.querySelector('.todo-input').value,
-        id: todos.length,
-        complete: false,
-        priority: document.querySelector('.priority').value,
-    }
-}))
+
+document.querySelector('.add-todo').addEventListener('click', ()=>{
+    const entry = {
+            text: document.querySelector('.todo-input').value,
+            id: todos.length,
+            complete: false,
+            priority: Number(document.querySelector('.priority').value),
+        }
+        addTodo(entry)
+        printTodo(entry)
+}
+    
+)
