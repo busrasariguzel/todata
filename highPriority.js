bd81c44a32feafcb61414426455ee3c2f2a742df
NikolayK <nikolay.kim@codeimmersive.com>
2019-12-05 11:55:45 -0500

    adds show priority

--- highPriority.js
+++ highPriority.js
@@ -13,3 +13,17 @@ document.querySelector('.high-tp-the-top-todo').addEventListener('click', functi
     currentTodos = priority2First(currentTodos)
     refreshTodos()
 })
+
+document.querySelector('.show-priority-todo').addEventListener('click', (event)=>{
+    currentTodos.forEach(entry => entry.text = entry.priority===1? 
+        !entry.text.includes('Low')
+        ?`${entry.text} - Low`:`${entry.text.slice(0,-5)}`
+        :!entry.text.includes('High')
+        ?`${entry.text} - High`:`${entry.text.slice(0,-6)}`)
+    refreshTodos()
+    if(event.target.innerText === 'SHOW PRIORITY'){
+        event.target.innerText = 'HIDE PRIORITY' 
+    }else {
+        event.target.innerText = 'SHOW PRIORITY'
+    }
+})
