b62b3799b18a1feffafc24f55c4424c4d7a0a056
busrasariguzel <busra.sariguzel@codeimmersives.com>
2019-12-05 10:47:03 -0500

    fixed the add function

--- functions.js
+++ functions.js
@@ -1,8 +1,11 @@
-document.querySelector('.add-todo').addEventListener('click', printTodo(function(){
-    return todo = {
+document.querySelector('.add-todo').addEventListener('click', addToDo(function(){
+    const entry ={
         text: document.querySelector('.todo-input').value,
         id: todos.length,
         complete: false,
         priority: document.querySelector('.priority').value,
     }
+    addToo(entry)
+    printTodo(entry)
 }))
+
